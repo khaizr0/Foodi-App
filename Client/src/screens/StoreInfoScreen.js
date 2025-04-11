abe1b0b05bf43460e7309b7a215972c350ad7940
Cao Phan Khải <54585187+khaizr0@users.noreply.github.com>
2025-04-11 21:50:25 +0700

    Update StoreInfoScreen.js

--- Client/src/screens/StoreInfoScreen.js
+++ Client/src/screens/StoreInfoScreen.js
@@ -5,29 +5,29 @@ export default function StoreInfoScreen() {
   const branches = [
     {
       id: "1",
-      title: "KFC Nguyễn Ánh Thủ",
+      title: "Xí Xọn Nguyễn Ánh Thủ",
       address:
-        "KFC Nguyễn Ánh Thủ 2, 787 Đ. Nguyễn Ánh Thủ, P, Quận 12, Hồ Chí Minh 70000",
+        "Xí Xọn Nguyễn Ánh Thủ 2, 787 Đ. Nguyễn Ánh Thủ, P, Quận 12, Hồ Chí Minh 70000",
       phone: "+1 234 567 890",
       hours: "10:00 AM - 10:00 PM",
       mapsLink:
-        "https://www.google.com/maps/dir//KFC+Nguy%E1%BB%85n+%E1%BA%A2nh+Th%E1%BB%A7+2,+787+%C4%90.+Nguy%E1%BB%85n+%E1%BA%A2nh+Th%E1%BB%A7,+P,+Qu%E1%BA%ADn+12,+H%E1%BB%93+Ch%C3%AD+Minh+70000/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x31752baa777cdde9:0xa20c43cf548305c1?sa=X&ved=1t:57443&ictx=111",
+        "https://www.google.com/maps/dir//Xí Xọn+Nguy%E1%BB%85n+%E1%BA%A2nh+Th%E1%BB%A7+2,+787+%C4%90.+Nguy%E1%BB%85n+%E1%BA%A2nh+Th%E1%BB%A7,+P,+Qu%E1%BA%ADn+12,+H%E1%BB%93+Ch%C3%AD+Minh+70000/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x31752baa777cdde9:0xa20c43cf548305c1?sa=X&ved=1t:57443&ictx=111",
     },
     {
       id: "2",
-      title: "KFC Lê Văn Sỹ",
-      address: "KFC Lê Văn Sỹ, 123 Đường Lê Văn Sỹ, Quận 3, Hồ Chí Minh, 70000",
+      title: "Xí Xọn Lê Văn Sỹ",
+      address: "Xí Xọn Lê Văn Sỹ, 123 Đường Lê Văn Sỹ, Quận 3, Hồ Chí Minh, 70000",
       phone: "+1 987 654 321",
       hours: "10:00 AM - 9:00 PM",
-      mapsLink: "https://www.google.com/maps/place/KFC+Le+Van+Sy",
+      mapsLink: "https://www.google.com/maps/place/Xí Xọn+Le+Van+Sy",
     },
     {
       id: "3",
-      title: "KFC Nguyễn Trãi",
-      address: "KFC Nguyễn Trãi, 456 Đường Nguyễn Trãi, Quận 1, Hồ Chí Minh, 70000",
+      title: "Xí Xọn Nguyễn Trãi",
+      address: "Xí Xọn Nguyễn Trãi, 456 Đường Nguyễn Trãi, Quận 1, Hồ Chí Minh, 70000",
       phone: "+1 555 666 777",
       hours: "10:00 AM - 10:30 PM",
-      mapsLink: "https://www.google.com/maps/place/KFC+Nguyen+Trai",
+      mapsLink: "https://www.google.com/maps/place/Xí Xọn+Nguyen+Trai",
     },
   ];
 
